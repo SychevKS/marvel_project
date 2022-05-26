7013a08dffda703cde5dd3e437f9f045cd21c2f9
Sychov Kirill <ksychov@pminst.ru>
2022-05-26 20:16:36 +0500

    пагинация

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=fbed79066bdc156d5c4979818714eaca';
+    _baseOffset = 210;
 
     getResouce = async (url) => {
         let res = await fetch(url);
@@ -13,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResouce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResouce(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
